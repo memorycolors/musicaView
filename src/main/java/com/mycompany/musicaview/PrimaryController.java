a2c5c87fdb3567cf86948f50f75f7a5d0b5a518c
Javi <Javi@DESKTOP-L6GL71T>
2020-05-08 14:06:00 +0200

    paso 7

--- src/main/java/com/mycompany/musicaview/PrimaryController.java
+++ src/main/java/com/mycompany/musicaview/PrimaryController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -19,7 +20,7 @@ import javax.persistence.Query;
 public class PrimaryController implements Initializable {
 
     private EntityManager entityManager;
-    private Titulo tituloSeleccionada;
+    private Cancion cancionSeleccionada;
     @FXML
     private TableColumn<Cancion, String> columnaTitulo;
     @FXML
@@ -61,13 +62,12 @@ public class PrimaryController implements Initializable {
                     }
                     return property;
                 });
-        tableViewContactos.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> {
-                    tituloSeleccionada = newValue;
-                    if (tituloSeleccionada != null) {
-                        textFieldTitulo.setText(tituloSeleccionada.getTitulo());
-                        textFieldAlbum.setText(tituloSeleccionada.getAlbum());
-                        textFieldCantante.setText(tituloSeleccionada.getCantante());
+        tableViewmusica.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                    cancionSeleccionada = newValue;
+                    if (cancionSeleccionada != null) {
+                        textFieldTitulo.setText(cancionSeleccionada.getTitulo());
+                       // textFieldAlbum.setText(cancionSeleccionada.getAlbum());
+                        textFieldCantante.setText(cancionSeleccionada.getCantante());
 
                     } else {
                         textFieldTitulo.setText("");
@@ -80,19 +80,19 @@ public class PrimaryController implements Initializable {
 
     @FXML
     private void onActionButtonGuardar(ActionEvent event) {
-        if (tituloSeleccionada != null) {
-            tituloSeleccionada.setTitulo(textFieldTitulo.getText());
-            tituloSeleccionada.setAlbum(textFieldAlbum.getText());
-            tituloSeleccionada.setCantante(textFieldCantante.getText());
+        if (cancionSeleccionada != null) {
+            cancionSeleccionada.setTitulo(textFieldTitulo.getText());
+           // cancionSeleccionada.setAlbum(textFieldAlbum.getText());
+            cancionSeleccionada.setCantante(textFieldCantante.getText());
             entityManager.getTransaction().begin();
-            entityManager.merge(tituloSeleccionada);
+            entityManager.merge(cancionSeleccionada);
             entityManager.getTransaction().commit();
 
-            int numFilaSeleccionada = tableViewContactos.getSelectionModel().getSelectedIndex();
-            tableViewContactos.getItems().set(numFilaSeleccionada, tituloSeleccionada);
-            TablePosition pos = new TablePosition(tableViewContactos, numFilaSeleccionada, null);
-            tableViewContactos.getFocusModel().focus(pos);
-            tableViewContactos.requestFocus();
+            int numFilaSeleccionada = tableViewmusica.getSelectionModel().getSelectedIndex();
+            tableViewmusica.getItems().set(numFilaSeleccionada, cancionSeleccionada);
+            TablePosition pos = new TablePosition(tableViewmusica, numFilaSeleccionada, null);
+            tableViewmusica.getFocusModel().focus(pos);
+            tableViewmusica.requestFocus();
         }
     }
 }
